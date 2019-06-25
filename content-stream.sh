d19bf47f84604a40f00fcb60a949d0675ad56030
root <root@cosmiccable.lan>
2019-06-25 03:35:56 +0200

    Add ffmpeg commandline for content streaming

--- content-stream.sh
+++ content-stream.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+#
+# Sidechain compression example.
+# Combines multiple compressors to achieve ratio of 60
+#
+# SIDECHAIN="sidechaincompress=threshold=0.003:ratio=20:attack=10:release=9000:knee=1:detection=peak"; ffmpeg -y -stream_loop -1 -i music.wav -i talk.wav -filter_complex '[1:a]asplit=4[talk1][talk2][talk3][talk4];[0:a][talk1]'$SIDECHAIN'[pause_a];[pause_a][talk2]'$SIDECHAIN'[pause_b];[pause_b][talk3]'$SIDECHAIN'[pause];[pause][talk4]amerge[final]' -map '[final]' -v trace -ac 1 final.wav && mpv final.wav
+
+
 exec ffmpeg \
 	-v error \
 	-i http://bassdrive.radioca.st \
